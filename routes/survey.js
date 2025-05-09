26d8b2666778c001dbb2de9c94126638224c14bc
sooyeon-26 <dltndus01226@naver.com>
2025-05-09 16:14:12 +0900

    콘솔로그삭제

--- routes/survey.js
+++ routes/survey.js
@@ -54,7 +54,7 @@ router.get("/", async (req, res) => {
 
   try {
     const surveys = await Survey.find({ approved: true });//일반유저는 승인된설문만 볼 수 있도록록
-    console.log("✅ 승인된 설문 조회 결과:", surveys);
+    //console.log("✅ 승인된 설문 조회 결과:", surveys);
     let userResponses = [];
     if (user?._id) {
       const foundUser = await User.findById(user._id);
