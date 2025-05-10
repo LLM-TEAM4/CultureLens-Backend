2b896ab9de631cd97a5ef0897f03abfd43ace919
sunny2you <ally1008@naver.com>
2025-05-11 05:45:29 +0900

    cookie console

--- routes/auth.js
+++ routes/auth.js
@@ -109,7 +109,7 @@ router.post("/login", async (req, res) => {
       profileImage: user.profileImage?.substring(0, 20) + "...(생략)",
       role: user.role,
     });
-
+    console.log(req.cookies);
     // ✅ 응답도 한 번만
     res.status(200).json({ 
       message: "로그인 성공",
